dd9fc3ff927c108746b771f2623783411b6c3355
alen james <alenjames899@gmail.com>
2025-10-04 12:13:57 +0530

    Create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import  GridBackground  from "./components/GridBackground";
 import "../src/styles/App.css";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
+    <>
+    
+    <NavBar/>
     <GridBackground>
       <Outlet />
-    </GridBackground>
+      
+    </GridBackground></>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import "../styles/NavBar.css"
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="navbar_container">
+        <div className="logo">
+            <h1>Alen James</h1>
+        </div>
+        <div className="nav_items">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Experience</li>
+                <li>contact Me</li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
