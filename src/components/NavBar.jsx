bbb021c70d5e684f05de2c31e7edbfbead409276
alen james <alenjames899@gmail.com>
2025-10-04 12:25:32 +0530

    Responsive Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import "../styles/NavBar.css"
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import "../styles/NavBar.css";
+
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <div className="navbar_container">
-        <div className="logo">
-            <h1>Alen James</h1>
-        </div>
-        <div className="nav_items">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Experience</li>
-                <li>contact Me</li>
-            </ul>
-        </div>
-    </div>
-  )
+    <nav className="navbar_container">
+      <div className="logo">
+        <h1>Alen James</h1>
+      </div>
+
+      {/* Hamburger icon for mobile */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+      </div>
+
+      <ul className={`nav_items ${isOpen ? "open" : ""}`}>
+        <li><NavLink to="/" end>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/skills">Skills</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
+        <li><NavLink to="/experience">Experience</NavLink></li>
+        <li><NavLink to="/contact">Contact Me</NavLink></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
