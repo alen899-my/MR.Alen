c580f1f54fdd4204f35820803d4e17f4f964891a
alen899-my <alenjames899@gmail.com>
2025-10-07 17:24:36 +0530

    moire icons

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -1,5 +1,5 @@
 // File: SkillsPage.jsx
-import React from 'react';
+
 import Tabs from './Tabs'; // import your Tabs component
 import '../styles/SkillsPage.css';
 
@@ -12,6 +12,17 @@ import js from "../assets/proglang/js.png"
 import Python from "../assets/proglang/Python.png"
 import Rpro from "../assets/proglang/Rpro.png"
 
+import Bootstrap from "../assets/frontend/Bootstrap.png"
+import css from "../assets/frontend/css.png"
+import html from "../assets/frontend/html.png"
+import jquery from "../assets/frontend/jquery.png"
+import React from "../assets/frontend/React.png"
+import Tailwindcss from "../assets/frontend/Tailwindcss.png"
+
+
+import asp from "../assets/backend/asp.png"
+import django from "../assets/backend/django.png"
+import nodejs from "../assets/backend/nodejs.png"
 
 const SkillsPage = () => {
   const skillTabs = [
@@ -31,20 +42,56 @@ const SkillsPage = () => {
       ),
     },
     {
-      id: 'tab-stack',
-      title: 'Tech Stack',
-      content: (
+    id: 'tab-stack',
+    title: 'Tech Stack',
+    content: (
         <div className="skill-icons">
-          
+          {/* Item 1: Bootstrap */}
+          <div className="skill_item">
+            <img src={Bootstrap} alt="Bootstrap Logo" />
+            <span>Bootstrap</span>
+          </div>
+
+          {/* Item 2: CSS */}
+          <div className="skill_item">
+            <img src={css} alt="CSS Logo" />
+            <span>CSS3</span>
+          </div>
+
+          {/* Item 3: HTML */}
+          <div className="skill_item">
+            <img src={html} alt="HTML Logo" />
+            <span>HTML5</span>
+          </div>
+
+          {/* Item 4: JQuery */}
+          <div className="skill_item">
+            <img src={jquery} alt="jQuery Logo" />
+            <span>jQuery</span>
+          </div>
+
+          {/* Item 5: React */}
+          <div className="skill_item">
+            <img src={React} alt="React Logo" />
+            <span>React</span>
+          </div>
+
+          {/* Item 6: Tailwind CSS */}
+          <div className="skill_item">
+            <img src={Tailwindcss} alt="Tailwind CSS Logo" />
+            <span>Tailwind CSS</span>
+          </div>
         </div>
-      ),
-    },
+    ),
+},
     {
       id: 'tab-design',
-      title: 'Designing',
+      title: 'Backend',
       content: (
         <div className="skill-icons">
-          
+          <div className="skill_item"><img src={asp} alt="JavaScript" /><span>asp dot net</span></div>
+          <div className="skill_item"><img src={django} alt="TypeScript" /><span>Django</span></div>
+          <div className="skill_item"><img src={nodejs} alt="Python" /><span>Node Js</span></div>
         </div>
       ),
     },
